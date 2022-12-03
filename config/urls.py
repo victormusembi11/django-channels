434d160bf14bf8eda6c072ff3d2f4661a81922e7
victormusembi11 <victor.musembi11@gmail.com>
2022-12-03 16:15:15 +0300

    refactor: url routes pattern changed

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^', include('example.urls', namespace='example')),
+    path('', include('app.urls')),
 ]
