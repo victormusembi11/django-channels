68c9a0bed3f51c953085795aa939321570816b91
victormusembi11 <victor.musembi11@gmail.com>
2022-12-03 22:48:14 +0300

    feat: list view update to retrieve a list of users to render

--- app/views.py
+++ app/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, get_user_model
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+User = get_user_model()
 
 
+@login_required(login_url='/log_in/')
 def user_list(request):
-    return render(request, 'example/user_list.html')
+    users = User.objects.select_related('logged_in_user')
+    for user in users:
+        user.status = 'Online' if hasattr(user, 'logged_in_user') else 'Offline'
+    return render(request, 'example/user_list.html', {'users': users})
 
 
 def log_in(request):
@@ -36,6 +43,7 @@ def sign_up(request):
     return render(request, 'example/sign_up.html', {'form': form})
 
 
+@login_required(login_url='/log_in/')
 def log_out(request):
     logout(request)
     return redirect(reverse_lazy('log_in'))
