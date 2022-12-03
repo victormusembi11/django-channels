1a3aa3accf68bc65dc8ccb7cb5dd6e50467c3f03
victormusembi11 <victor.musembi11@gmail.com>
2022-12-03 22:44:53 +0300

    feat: send msg after a client connects & before a client disconnects

--- app/consumers.py
+++ app/consumers.py
@@ -1,9 +1,25 @@
 from consumers import Group
+import json
+from channels.auth import channel_session_user, channel_session_user_from_http
 
 
+@channel_session_user_from_http
 def ws_connect(message):
     Group('users').add(message.reply_channel)
+    Group('users').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': True
+        })
+    })
 
 
+@channel_session_user
 def ws_disconnect(message):
+    Group('users').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': False
+        })
+    })
     Group('users').discard(message.reply_channel)
