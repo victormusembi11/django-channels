b73f538d554acfdc253a032534de74f9f8cb609c
victormusembi11 <victor.musembi11@gmail.com>
2022-12-03 17:48:30 +0300

    feat: login page for user to auth w/ a uname & passwd

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import user_list
+from .views import user_list, log_in, log_out
 
 
 urlpatterns = [
     path('users/', user_list, name='user_list'),
+    path('login/', log_in, name='log_in'),
+    path('logout/', log_out, name='log_out'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 
 def user_list(request):
     return render(request, 'example/user_list.html')
+
+
+def log_in(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('user_list')
+        else:
+            print(form.errors)
+    
+    return render(request, 'example/log_in.html', {'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse_lazy('log_in'))
